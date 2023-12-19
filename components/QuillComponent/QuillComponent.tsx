98dcf8fe789f19fd49355ac512302eb22489b7bc
InefableKoumba <iloveyoujulina@gmail.com>
2023-12-19 17:51:22 +0100

    Update dependencies and add image upload functionality

--- components/QuillComponent/QuillComponent.tsx
+++ components/QuillComponent/QuillComponent.tsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from 'react';
 import { useQuill } from 'react-quilljs';
 import BlotFormatter from 'quill-blot-formatter';
+import { v4 as uuidv4 } from 'uuid';
 import 'quill/dist/quill.snow.css';
-
 
 function base64ToBlob(base64String: string) {
   const parts = base64String.split(';base64,');
@@ -16,6 +16,7 @@ function base64ToBlob(base64String: string) {
   const blob = new Blob([byteArrays], { type });
   return blob;
 }
+
 function createImageElement(blob: Blob | MediaSource) {
   const imageUrl = URL.createObjectURL(blob);
   const imageElement = document.createElement('img');
@@ -23,8 +24,29 @@ function createImageElement(blob: Blob | MediaSource) {
   return imageElement;
 }
 
+async function extractBlobAndCreateFile(imageElement: HTMLImageElement, extension: string | null) {
+  const imageUrl = imageElement.src
+  console.log("imageUrl")
+  if (imageUrl) {
+    // Fetch Blob data
+    const response = await fetch(imageUrl);
+    const blobData = await response.blob();
+
+    // Create File
+    const id = uuidv4();
+    const file = new File([blobData], id + + '.' + extension, { type: blobData.type });
+    console.log("Here is my file")
+    console.log(file)
+    return file;
+  } else {
+    console.error('Blob URL not found in the provided string');
+    return null;
+  }
+}
+
+
 const QuillEditorComponent = ({ post }: { post: Post | null }) => {
-  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
+  const [uploadedImages, setUploadedImages] = useState<any[]>([]);
   const { quill, quillRef, Quill } = useQuill({
     modules: { blotFormatter: {} }
   });
@@ -42,26 +64,33 @@ const QuillEditorComponent = ({ post }: { post: Post | null }) => {
         const newImages = delta.ops?.filter((op) => op.insert && op.insert.image)
           .map((op) => op.insert.image);
 
+        console.log("newImages")
+        let extension = null
+        if (newImages) {
+          const mediaType = newImages[0].split(';')[0];
+          extension = mediaType.split('/')[1];
+        }
+
+        let imageElement = null
+
         newImages?.forEach((image) => {
           const selection = quill.getSelection();
           if (selection) {
             const position = selection.index; // Access the 'index' property
             const blob = base64ToBlob(image);
-            const imageElement = createImageElement(blob);
-            quill.insertEmbed(position, 'image', imageElement.src);
+            const data = createImageElement(blob);
+            imageElement = data;
           }
         });
-        
-        console.log("newImages")
-        console.log(newImages)
-        // Add new images to the existing uploaded images
-        if (newImages)
-          setUploadedImages((prevImages) => [...prevImages, ...newImages]);
-        console.log("UploadedImages")
-        console.log(uploadedImages)
 
-        let currrentContents = quill.getContents();
-        console.log(currrentContents.diff(oldContents));
+        let file;
+        if (imageElement) {
+          extractBlobAndCreateFile(imageElement, extension).then((res) => {
+            file = res;
+            console.log("file")
+            console.log(file)
+          })
+        }
       });
     }
   }, [quill, Quill]);
